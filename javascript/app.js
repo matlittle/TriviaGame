9f648872eaeb8f7d70d83f582304a39617837e17
matlittle <matlittle727@gmail.com>
2017-10-15 10:45:30 -0500

    Function created to pick random unasked question

--- javascript/app.js
+++ javascript/app.js
@@ -1,81 +1,112 @@
 // initialize variables needed to begin the game
-
-// object to hold stats (correct/incorrect/skipped)
-var stats = {
-	correct: 0,
-	incorrect: 0,
-	unanswered: 0
-}
-
 var trivia = {
 	// object to hold questions, choices, and correct answer
 	one: {
 		question: "A",
 		answer: "A",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	two: {
 		question: "B",
 		answer: "B",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	three: {
 		question: "C",
 		answer: "C",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	four: {
 		question: "D",
 		answer: "D",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	five: {
 		question: "A",
 		answer: "A",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	six: {
 		question: "B",
 		answer: "B",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	seven: {
 		question: "C",
 		answer: "C",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	eight: {
 		question: "D",
 		answer: "D",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	nine: {
 		question: "A",
 		answer: "A",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	},
 	ten: {
 		question: "B",
 		answer: "B",
 		choices: ["A","B","C","D"],
-		order: 
+		asked: false 
 	}
 }
 
-var mainDiv = $("#content");
+var contentDiv = $("#content");
+
+
 
 // show initial starting screen with option to begin trivia game
+function showStartPage() {
+	var startDiv = $("<div>").className("row")
+	var startBtn = $("<h2>").attr("id", "start-btn").text("Start");
+
+	myAppend(startBtn, startDiv, contentDiv);
+
+	$(startBtn).click(function() {
+		$(contentDiv).html("");
+		startGame();
+	});
+}
 
 // once user clicks start, initialize the game
+function startGame() {
+
+	// object to hold stats (correct/incorrect/skipped)
+	var stats = {
+		correct: 0,
+		incorrect: 0,
+		unanswered: 0
+	}
+
+	function getRandomQuestion() {
+		//get array of trivia object keys
+		var keys = Object.keys('trivia');
+
+		// get possible trivia questions where asked is false
+		var possQuestions = jQuery.grep(keys, function(question){
+			return !question.asked;
+		});
+
+		// get random number to select possible question
+		randomNum = Math.floor(Math.random() * possQuestion.length);
+		
+		// return question
+		return possQuestions[randomNum];
+	}
+
+}
 
 	// show first question and start the timer
 
@@ -97,3 +128,13 @@ var mainDiv = $("#content");
 // after all questions have been asked
 // show final stats page with number of correct/incorrect/skipped answers
 // include button to restart game. 
+
+
+// Function to append elements together
+function myAppend() {
+	for(var i = 0; i < arguments.length - 1; i += 1) {
+		$(arguments[i+1]).append(arguments[i]);
+	}
+}
+
+
